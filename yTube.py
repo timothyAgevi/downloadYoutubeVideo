af97b2b496c82052a3f3e0c6941f8bdb79243f7e
timothyAgevi <timothyagevi@gmail.com>
2021-08-03 18:20:34 +0300

    click function

--- yTube.py
+++ yTube.py
@@ -1,3 +1,21 @@
 from tkinter import *
 import pytube
 from tkinter import ttk
+
+win = Tk()
+win.title( 'Youtube downloader')
+win.geometry('522x600')
+win.iconbitmap('yt.ico') # This changes the icon of the tkinter window
+win.config(bg = 'red')
+
+#add an image
+photo1 = PhotoImage(file = 'youtube-logo.png')
+Label(win, image = photo1, bg = 'red').grid(row=0, column=0)
+
+url = ' '
+
+def click():
+    url = entry.get()
+    video = pytube.YouTube(url)
+    youtube = video.streams.first()
+    youtube.download(r'C:\Users\Ugoboss\Desktop\Youtube')
